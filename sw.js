1f013359e11dce5c08f85ad276822b086e98d412
onetapmeme <onetapmemecoin@gmail.com>
2025-08-24 02:45:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,28 @@
-const CACHE = 'onetap-v2';
-const CORE = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './assets/onetap_logo.png'
+const CACHE = 'onetap-v4.3';
+const PRECACHE = [
+  './','./index.html','./style.css','./script.js','./manifest.json',
+  './assets/onetap_logo.png','./assets/tap2enter_pc_static.png'
 ];
 
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting()));
 });
+
 self.addEventListener('activate', e=>{
   e.waitUntil((async ()=>{
     const keys = await caches.keys();
-    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    await Promise.all(keys.filter(k => k!==CACHE).map(k=>caches.delete(k)));
     await self.clients.claim();
   })());
 });
+
 self.addEventListener('fetch', e=>{
+  const req = e.request;
   e.respondWith((async ()=>{
     const cache = await caches.open(CACHE);
-    const cached = await cache.match(e.request);
-    const fetchPromise = fetch(e.request).then(res=>{
-      if(res && res.status===200 && e.request.method==='GET'){
-        cache.put(e.request, res.clone());
-      }
+    const cached = await cache.match(req);
+    const fetchPromise = fetch(req).then(res=>{
+      if(res && res.status===200 && req.method==='GET'){ cache.put(req, res.clone()); }
       return res;
     }).catch(()=>cached);
     return cached || fetchPromise;
